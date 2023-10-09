32edb7e6dc1ce85aa265b1675713f9660cdfc57f
Shaira Osmani <shairashegarf@gmail.com>
2023-10-09 12:07:27 -0400

    Delete BrandnameGenerator.py

--- BrandnameGenerator.py
+++
@@ -1,5 +0,0 @@
-#Day 1/100 -> Project 1
-# Create a program that would take the name of your city and 
-# will ask you the name of your pet and will generate a band name.
-# Remember to take the input in the new line.
-
